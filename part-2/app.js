1aa6a7c7c8632a509d1b22958cb6e5bec47d877f
Glenn-David Daniel <d2rd@outlook.com>
2017-08-01 15:10:21 -0700

    App reads clients file and displays in browser

--- part-2/app.js
+++ part-2/app.js
@@ -4,8 +4,9 @@ var clients = fs.readFileSync('./part-2/clients.json', 'utf8');  //reads the cli
 
 var server = http.createServer(function(req, res){
   console.log('request was made: ' + req.url);
-
-res.writeHead(200, {'Content-Type': 'application/json'});
-
-res.end(JSON.stringify(clients));
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(clients)); //converts array to string
 });
+
+server.listen(3000, '127.0.0.1'); // server is listening on port 3000
+console.log('now listening to port 3000');
--- part-2/searchByCompany.js
+++ part-2/searchByCompany.js
@@ -1,16 +1,6 @@
 // searchByCompany.js takes one string argument (let's call it company), finds all the clients from clients.json who have a company that starts with the company string, and prints the id, company and phone of each matching client.
 // Your searches should not be case-sensitive: i.e. a search for "Boston" is the same as a search for "boston".
 
-var http = require('http');
-var fs = require('fs');
-
-// var clients = fs.readFileSync('clients.json', 'utf8');  //reads the clients.json file
-// var clients = require('./clients.json');
-// var data = clients;
-
-var server = http.createServer(function(req, res) {
-    console.log('request was made: ' + req.url)
-    res.writeHead(200, { 'Content-Type': 'application/json' });
     var myObj = {
         name: 'Ryu',
         job: 'Ninja',
@@ -19,11 +9,12 @@ var server = http.createServer(function(req, res) {
     res.end(JSON.stringify(myObj));
 
 });
-server.listen(3000, '127.0.0.1');
-console.log('now listening to port 3000');
+
 function searchByCompany(data, val) {
 var objects
 }
 console.log(clients);
+
+
 
 module.export = searchByCompany.js;
