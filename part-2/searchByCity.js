7836bc99bceadec8f000fb2db8c6ea35590710d2
Glenn-David Daniel <d2rd@outlook.com>
2017-08-01 22:16:54 -0700

    WIP displays clients.json.  Needs filter.

--- part-2/searchByCity.js
+++ part-2/searchByCity.js
@@ -1,3 +1,13 @@
 // searchByCity.js takes one string argument (let's call it the city), finds all the clients from clients.json who have a city that exactly matches the city, and prints the id, rep_name, company, city, and state of each matching client.
 
 // Your searches should not be case-sensitive: i.e. a search for "Boston" is the same as a search for "boston".
+
+for(i =0; i < clients.length;i++){ 
+        // 'i' is index for iteration. length is a property of the array that indicates the number of objects in the array.'i++' increments the iteration by 1.
+          document.write('<h4>' + clients[i].id + '</h4>');
+          document.write('<li>' + clients[i].rep_name + '</li>');
+          document.write('<li>' + clients[i].company + '</li>');
+          document.write('<li>' + clients[i].city + '</li>');
+          document.write('<li>' + clients[i].state + '</li>');
+        } 
+        
--- part-2/searchByCompany.js
+++ part-2/searchByCompany.js
@@ -1,20 +1,13 @@
 // searchByCompany.js takes one string argument (let's call it company), finds all the clients from clients.json who have a company that starts with the company string, and prints the id, company and phone of each matching client.
 // Your searches should not be case-sensitive: i.e. a search for "Boston" is the same as a search for "boston".
 
-    var myObj = {
-        name: 'Ryu',
-        job: 'Ninja',
-        age: 29
-    };
-    res.end(JSON.stringify(myObj));
-
-});
-
-function searchByCompany(data, val) {
-var objects
-}
-console.log(clients);
-
+for(i =0; i < clients.length;i++){ 
+        // 'i' is index for iteration. length is a property of the array that indicates the number of objects in the array.'i++' increments the iteration by 1.
+          document.write('<h4>' + clients[i].id + '</h4>');
+          document.write('<li>' + clients[i].company + '</li>');
+          document.write('<li>' + clients[i].phone + '</li>');
+       // document.write('<li>' + clients[i].rep_name'</li>';
+        } 
 
 
 module.export = searchByCompany.js;
