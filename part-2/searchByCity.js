5251acfab75623b40454fe3741f1c5516e13491a
Glenn-David Daniel <d2rd@outlook.com>
2017-08-09 12:08:42 -0700

    Baseline for searchByCity feature.

--- part-2/searchByCity.js
+++ part-2/searchByCity.js
@@ -2,44 +2,45 @@
 
 // Your searches should not be case-sensitive: i.e. a search for "Boston" is the same as a search for "boston".
 
-<<<<<<< HEAD
 //Pseudo.
-
 // 1. Get required resources
-        var fs = require('fs');  //requires the node 'fs' library.
-        var clients = fs.readFileSync('./clients.json', 'utf8');  //loads the clients.json file into the var 'clients' (using absolute path).
-        var targetCityNameProcess = [process.argv[2]]  //search parameter is the 3rd array item
-// 2.  
-console.log(targetCityNameProcess);  // checks value is stored by process.argv
-function byCityName(targetCityName){
+var fs = require('fs'); //requires the node 'fs' library.
+var clientData = require('./clients')
+var targetCompany = process.argv[2] //search parameter is the 3rd array item.
+console.log(targetCompany);
 
-  // Setup loop 
-          //return an array of values that match on a certain key
-              function getValues(clients, key) {
-                  var clientsFound = [];
-                  for (var i in clients) {
-                      if (!clients.hasOwnProperty(i)) continue;
-                      if (typeof clients[i] == 'object') {
-                          clientsFound = clientsFound.concat(getValues(clients[i], key));
-                      } else if (i == key) {
-                          clientsFound.push(clients[i]);
-                      }
-                  }
-                  return clientsFound;
-              }
+// 2. load the clients.json file into the var 'clientData' (using absolute path).
+// const clientData = JSON.parse(fs.readFileSync('./clients.json', 'utf8')); //
+
+function byCompany(clientObj, index, clientData) { 
+    // console.log(clientObj);   
+  if (clientObj.company == targetCompany) {
+        return true;
+    }
+    else {
+         return false;
+     }}
+
+var clientsFound = clientData.filter(byCompany);
+// console.log(JSON.parse(clientsFound,));
+console.log(clientsFound); // print all the values of clientsFound
+
+for (i in clientsFound){
+    console.log(i);
+    for (key in clientsFound[i]){
+      if(key !== 'no_employees' && key !== 'phone' && key !== 'created_at') { // filters out unwanted keys.  NOTE: Must use '&&'!  '||' ("OR" operator) does not work in JS 'if'.  This is NOT SQL.
+        console.log( key + ": " + clientsFound[i][key]);
+      }
+       
+        
+    }
 }
 
-console.log(byCityName());
+// Return these values
+        // clientsFound.id
+        // clientsFound.rep_name
+        // clientsFound.company
+        // clientsFound.city
+        // clientsFound.state
 
-module.exports.byCityName = byCityName;
-=======
-for(i =0; i < clients.length;i++){ 
-        // 'i' is index for iteration. length is a property of the array that indicates the number of objects in the array.'i++' increments the iteration by 1.
-          document.write('<h4>' + clients[i].id + '</h4>');
-          document.write('<li>' + clients[i].rep_name + '</li>');
-          document.write('<li>' + clients[i].company + '</li>');
-          document.write('<li>' + clients[i].city + '</li>');
-          document.write('<li>' + clients[i].state + '</li>');
-        } 
         
->>>>>>> cecaf6688ac221c31f77822eb5a977f10d1fc890
